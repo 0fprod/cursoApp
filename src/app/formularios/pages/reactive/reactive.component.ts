3a7b5f22880a4858c646afa67607b4320599f882
Palacios Rodriguez <francisco-javier.palaciosrodriguez.external@atos.net>
2018-03-20 14:37:01 +0000

    reactive

--- src/app/formularios/pages/reactive/reactive.component.ts
+++ src/app/formularios/pages/reactive/reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Alumno } from '../../../shared/models/alumno';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  private options:any[] = ['op1', 'op2'];
+
+  private alumnoForm:FormGroup;
+  private alumno: Alumno;
+  private selectedOp: any;
+
+  constructor(private fb:FormBuilder) { 
+  }
 
   ngOnInit() {
+    this.alumnoForm = this.fb.group({
+      id : [0, Validators.required],
+      name: ['', [Validators.required, Validators.pattern(/A\d{6}/g)] ],
+      email: ['', [Validators.required, Validators.email]],
+      opt: ''
+    });
+    this.onChanges();
+  }
+
+
+  onChanges(): void{
+    this.alumnoForm.valueChanges.subscribe(val => {
+      console.log(val);
+      //val es el contenido del alumnoForm en cada momento que cambie alguno de sus campos
+      //para acceder desde el html es poner en un input [(ngModel)]="alumnoForm.value.campoquequieras"
+    });
+  }
+
+  onSubmit(){
+    this.alumno = new Alumno(
+      this.alumnoForm.get('id').value,
+      this.alumnoForm.get('name').value,
+      this.alumnoForm.get('email').value,
+    );
   }
 
 }
